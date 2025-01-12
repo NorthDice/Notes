17ad6c5c42adb524fc52f5b6298a84f7b66e9f99
Balychev Kiril <mister.balychev@gmail.com>
2025-01-12 13:59:57 +0200

    Created new controller and class notes

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,19 @@
+﻿namespace Notes.Models
+{
+    public class Note
+    {
+        public Note(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
